8287f5fdec9e68d1bb3c7542cbdd657e8a06e16a
Ahmed <ahmedmegz864@gmail.com>
2023-10-25 21:45:45 +0300

    implement Average feature

--- Assignment.cpp
+++ Assignment.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 // Function declaration
 int getSum(vector<int> vector1);
+float getAverage(vector<int> vector1);
 
 // Main function
 int main()
 {
   vector<int> vector1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   cout << "The sum of the numbers is: " << getSum(vector1) << endl;
+  cout << "The average of the numbers is: " << getAverage(vector1) << endl;
   return 0;
 }
 
@@ -21,4 +23,13 @@ int getSum(vector<int> vector1)
     sum += vector1[i];
   }
   return sum;
+}
+float getAverage(vector<int> vector1)
+{
+  float sum = 0;
+  for (int i = 0; i < vector1.size(); i++)
+  {
+    sum += vector1[i];
+  }
+  return (sum / vector1.size());
 }
