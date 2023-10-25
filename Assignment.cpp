ad29ff1aa3e442442d9fba5f90ee0fde09ac92b8
Ahmed <ahmedmegz864@gmail.com>
2023-10-25 21:31:12 +0300

    implement getSum feature

---
+++ Assignment.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// Function declaration
+int getSum(vector<int> vector1);
+
+// Main function
+int main()
+{
+  vector<int> vector1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  cout << "The sum of the numbers is: " << getSum(vector1) << endl;
+  return 0;
+}
+
+// Function definition
+int getSum(vector<int> vector1)
+{
+  int sum = 0;
+  for (int i = 0; i < vector1.size(); i++)
+  {
+    sum += vector1[i];
+  }
+  return sum;
+}
